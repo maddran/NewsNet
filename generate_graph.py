323f685408f23388b3f0ee3f9d366a7a9732c1bc
maddran <madhavnarendran@gmail.com>
2020-12-03 23:05:58 +0200

    Added generate_graph.py

--- generate_graph.py
+++ generate_graph.py
@@ -4,7 +4,7 @@ import os, sys
 
 def get_links(file):
     df = pd.read_pickle(file)
-    df_articles = pd.DataFrame(df['parsed_article'].values, index = df.index)
+    df_articles = pd.DataFrame(df['parsed_article'].values.tolist(), index = df.index)
     # df = df.loc[:,['index','top_level_domain', 'external_links']]
     print(df_articles.columns)
     # print(df.explode('enternal_links').head(5))
