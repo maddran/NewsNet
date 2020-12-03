8bc9c02444bff92be6d478c185a3837ab24ca217
maddran <madhavnarendran@gmail.com>
2020-12-03 23:22:13 +0200

    Added generate_graph.py

--- generate_graph.py
+++ generate_graph.py
@@ -8,7 +8,7 @@ def get_links(file):
     df_articles = pd.DataFrame(df['parsed_article'].values.tolist(), index = df.index)
     df_res = pd.concat([df.loc[:,['index', 'top_level_domain']],
                         df_articles.loc[:,['internal_links', 'parsed_date']]], axis = 1)
-    print(df_res.head()
+    print(df_res.head())
     # print(df.explode('enternal_links').head(5))
 
 def get_source_tld():
