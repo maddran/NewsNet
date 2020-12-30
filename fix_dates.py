ad9c4c33692933621e0f3f7d0f1f999ab3bf04a7
maddran <madhavnarendran@gmail.com>
2020-12-30 10:53:36 -0700

    update fix_date

--- fix_dates.py
+++ fix_dates.py
@@ -7,15 +7,16 @@ from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 
 def fix_dates(file):
+    new_file = f"{file.split('.pkl')[0]}_fixed_date.pkl"
+    if os.path.exists(new_file):
+        return
+        
     df = pd.read_pickle(file)
 
     tqdm.pandas(desc=file)
     df = df.progress_apply(apply_fix, axis = 1)
-
-    # new_file = f"{file.split('.pkl')[0]}_fixed_date.pkl"
-    # df.to_pickle(new_file)
-
-    df.to_pickle(file)
+    
+    df.to_pickle(new_file)
 
 def apply_fix(row):
     if row['parsed_article']:
