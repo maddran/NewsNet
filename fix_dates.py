46e7c49281b5201e679cfc046720d34eb01d39a2
maddran <madhavnarendran@gmail.com>
2020-12-29 21:15:45 -0700

    add fix_dates

---
+++ fix_dates.py
@@ -0,0 +1,32 @@
+from parse_articles import parse_date
+import argparse
+import os
+import sys
+iport pandas as pd
+
+def fix_dates(file):
+    df = pd.read_pickle(file)
+    df = df.apply(appl_fix)
+    df = pd.to_pickle(f"fixed_{file}")
+
+def apply_fix(row):
+    if row['parsed_article']['parsed_date']:
+        return row
+    else:
+        row['parsed_article']['parsed_date'] = parse_date(row['parsed_article']['url'])
+        return row
+
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return arg
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parsed_files", required=True, type=lambda x: is_valid_file(parser, x),
+                        nargs='+', help="full path of the parsed files to generate a network from")
+
+    args = parser.parse_args()
+
+    _ = [fix_dates(i, file) for i, file in enumerate(args.parsed_files)]
