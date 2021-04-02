e2f41ce17f781681a5dd8502bdaac25f7645ea62
maddran <madhavnarendran@gmail.com>
2021-04-02 01:15:46 -0600

    update text_classification.py

--- text_helpers.py
+++ text_helpers.py
@@ -111,6 +111,6 @@ def predict_pipeline(text, model_path='news_classifier.pt'):
                     for i in range(len(predictions))]
     pred_cats2 = np.concatenate(pred_cats2).ravel()
 
-    print(predictions[0].shape, len(flat_preds))
+    print(predictions[0].shape, len(text))
 
     return pred_cats1, pred_cats2
