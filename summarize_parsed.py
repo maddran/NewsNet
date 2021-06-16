80638f895fbb5d2333fac2c658c2a1e78da8c9a1
maddran <madhavnarendran@gmail.com>
2021-06-15 23:24:49 -0600

    update summarized_parsed to group by day

--- summarize_parsed.py
+++ summarize_parsed.py
@@ -3,6 +3,7 @@ import pickle
 from glob import glob
 from itertools import groupby
 from parse_articles import cwd
+import numpy as np
  
 parsed_files = sorted(glob(f"{cwd()}/parsed/*parsed.pkl"))
 if not parsed_files:
@@ -11,6 +12,11 @@ if not parsed_files:
 grouped_files = [list(g) for k, g in groupby(parsed_files, key=lambda x: x.split('/')[-1][:8])]
 
 for group in grouped_files:
+    num_articles = []
+    success_rate = []
+    date_parse_rate = []
+    text_parse_rate = []
+
     for pf in group:    
         try:
             df = pd.read_pickle(pf)
@@ -21,19 +27,19 @@ for group in grouped_files:
 
         parsed = pd.DataFrame([p for p in list(parsed) if p])
         parsed['lang'] = df['lang_short']
-        num_articles = len(parsed)
-        success_rate = num_articles*100/total_articles
-        date_parse_rate = sum(pd.notna(parsed['parsed_date']))*100/total_articles
-        text_parse_rate = sum(pd.notna(parsed['text']))*100/total_articles
+        num_articles.append(len(parsed))
+        success_rate.append(num_articles*100/total_articles)
+        date_parse_rate.append(sum(pd.notna(parsed['parsed_date']))*100/total_articles)
+        text_parse_rate.append((pd.notna(parsed['text']))*100/total_articles)
 
-        print("\n\n", parsed.columns)
+    # print("\n\n", parsed.columns)
 
-        sep = "-"*20
-        print(f"\n{sep}{pf.split('/')[-1][:8]}{sep}")
-        print(f"# of articles parsed = {num_articles}")
-        print(f"parse success rate = {round(success_rate,2)}%")
-        print(f"date parse success rate = {round(date_parse_rate,2)}%")
-        print(f"text parse success rate = {round(text_parse_rate,2)}%")
+    sep = "-"*20
+    print(f"\n{sep}{pf.split('/')[-1][:8]}{sep}")
+    print(f"# of articles parsed = {np.mean(num_articles)}")
+    print(f"parse success rate = {round(np.mean(success_rate),2)}%")
+    print(f"date parse success rate = {round(np.mean(date_parse_rate),2)}%")
+    print(f"text parse success rate = {round(np.mean(text_parse_rate),2)}%")
 
 
     
