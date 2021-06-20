ec6df20ee1d1a59f910817ae416b7aeda759899b
maddran <madhavnarendran@gmail.com>
2021-06-20 16:55:48 -0600

    fix generate_edgelist.py save to pickle

--- generate_edgelist.py
+++ generate_edgelist.py
@@ -109,5 +109,5 @@ if __name__ == "__main__":
     print(f"\n{len(links_df)} total links found. {round(parsed_date_prop,2)}% of publish dates found.")
 
     now = datetime.now().strftime("%Y%m%d_%H%M%S")
-    links_df.to_pickle(f"edgelist/edgelist_{now}.pkl",index=False)
+    links_df.to_pickle(f"edgelist/edgelist_{now}.pkl")
     print(links_df.sample(10))
